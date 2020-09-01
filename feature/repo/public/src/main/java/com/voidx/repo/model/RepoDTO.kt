c8968666033642802d46e3db0e7200ea05a5e9bb
Victor Fernandes <victorfernandes91@hotmail.com>
2020-08-31 21:00:01 -0300

    adding parcelable to user and repo dtos

--- feature/repo/public/src/main/java/com/voidx/repo/model/RepoDTO.kt
+++ feature/repo/public/src/main/java/com/voidx/repo/model/RepoDTO.kt
@@ -1,16 +1,22 @@
 package com.voidx.repo.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import com.voidx.repo.BR
 import com.voidx.user.domain.model.UserDTO
 
-class RepoDTO : BaseObservable() {
+class RepoDTO() : BaseObservable(), Parcelable {
 
     var id: Int = -1
 
     @Bindable
-    var name: String = ""
+    var name: String? = null
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.name)
+        }
 
     @Bindable
     var description: String? = null
@@ -39,5 +45,32 @@ class RepoDTO : BaseObservable() {
             field = value
             notifyPropertyChanged(BR.owner)
         }
+
+    constructor(parcel: Parcel) : this() {
+        id = parcel.readInt()
+        name = parcel.readString()
+        description = parcel.readString()
+        forksCount = parcel.readInt()
+        starsCount = parcel.readInt()
+        owner = parcel.readParcelable(UserDTO::class.java.classLoader)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RepoDTO> {
+        override fun createFromParcel(parcel: Parcel): RepoDTO {
+            return RepoDTO(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RepoDTO?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }
--- feature/user/public/src/main/java/com/voidx/user/domain/model/UserDTO.kt
+++ feature/user/public/src/main/java/com/voidx/user/domain/model/UserDTO.kt
@@ -1,24 +1,53 @@
 package com.voidx.user.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import com.voidx.user.BR
 
-class UserDTO : BaseObservable() {
+class UserDTO() : BaseObservable(), Parcelable {
 
     var id: Int = -1
 
     @Bindable
-    var login: String = ""
+    var login: String? = null
         set(value) {
             field = value
             notifyPropertyChanged(BR.login)
         }
 
     @Bindable
-    var avatar: String = ""
+    var avatar: String? = null
         set(value) {
             field = value
             notifyPropertyChanged(BR.avatar)
         }
+
+    constructor(parcel: Parcel) : this() {
+        id = parcel.readInt()
+        login = parcel.readString()
+        avatar = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(login)
+        parcel.writeString(avatar)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserDTO> {
+        override fun createFromParcel(parcel: Parcel): UserDTO {
+            return UserDTO(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserDTO?> {
+            return arrayOfNulls(size)
+        }
+    }
+
 }
